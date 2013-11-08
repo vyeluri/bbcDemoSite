0ce81c150126104810554724a4a5d4abdd258a5e
vyeluri <vyeluri5@gmail.com>
2013-11-08 16:33:23 +1100

    Redit email func

--- App_Code/SendMail.cs
+++ App_Code/SendMail.cs
@@ -21,17 +21,17 @@ public class SendMail
               MailMessage msg = new MailMessage();
               SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
 
-               msg.From = new MailAddress("");
+               msg.From = new MailAddress("testgjava@gmail.com");
                msg.To.Add(new MailAddress(to));
                msg.Subject = sub;
-               msg.Bcc.Add(new MailAddress(""));
+               msg.Bcc.Add(new MailAddress("venkatesh.vm115@gmail.com"));
                msg.Subject = msgSub;
                msg.Body = "Hi," + name + "<br /><br /> Thank You for contacting us. We will contact you soon. <br />" + "Our System has recorded your details <br />" + "Contact No: " + sub + "<br/>" + panLabel+"" + panBox + "<br/>" + "Address: " + addr + " <br /><br /> Regards <br />V!";
                msg.IsBodyHtml = true;
 
                //Name the client which you will be using to send email.
                SmtpServer.Port = 587;
-               SmtpServer.Credentials = new System.Net.NetworkCredential("", "");
+               
                SmtpServer.EnableSsl = true;
 
                SmtpServer.Send(msg);
